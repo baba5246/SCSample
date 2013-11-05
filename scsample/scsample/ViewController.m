a3c3959f55b11c2357cd71c4b82e1981abe16e7e
baba5246 <baba_writer@yahoo.co.jp>
2013-11-05 17:21:04 +0900

    再生・一時停止・停止機能追加

--- scsample/scsample/ViewController.m
+++ scsample/scsample/ViewController.m
@@ -4,6 +4,9 @@
 #import "SCAPI.h"
 
 @interface ViewController ()
+{
+    AVAudioPlayer *player;
+}
 
 @end
 
@@ -15,10 +18,44 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
+    CGFloat cumulo_height = self.view.frame.size.height - 150;
+    
+    UIButton *play = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [play setFrame:CGRectMake(20, cumulo_height, self.view.frame.size.width-40, 30)];
+    [play setTitle:@"Play" forState:UIControlStateNormal];
+    [play addTarget:self action:@selector(playSong) forControlEvents:UIControlEventTouchUpInside];
+    
+    cumulo_height += play.frame.size.height + 10;
+    
+    UIButton *pause = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [pause setFrame:CGRectMake(20, cumulo_height, self.view.frame.size.width-40, 30)];
+    [pause setTitle:@"Pause" forState:UIControlStateNormal];
+    [pause addTarget:self action:@selector(pauseSong) forControlEvents:UIControlEventTouchUpInside];
+    
+    cumulo_height += pause.frame.size.height + 10;
+    
+    UIButton *stop = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [stop setFrame:CGRectMake(20, cumulo_height, self.view.frame.size.width-40, 30)];
+    [stop setTitle:@"Stop" forState:UIControlStateNormal];
+    [stop addTarget:self action:@selector(stopSong) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:play];
+    [self.view addSubview:pause];
+    [self.view addSubview:stop];
+    
+}
+
+- (void) playSong
+{
+    // 時間計測開始
+    NSDate *started = [NSDate date];
+    
+    // URL指定
     NSString *apiUrl = @"https://api.soundcloud.com/tracks/%@/stream?client_id=%@";
     NSString *songId = @"13678407";
     NSString *clientId = @"e1473e819ae2406e130bdff0c8087e7f";
     
+    // Request
     NSString *urlString = [NSString stringWithFormat:apiUrl, songId, clientId];
     [SCRequest performMethod: SCRequestMethodGET
                   onResource: [NSURL URLWithString:urlString]
@@ -27,12 +64,31 @@
       sendingProgressHandler: nil
              responseHandler:^(NSURLResponse *response, NSData *data, NSError *error){
                  
-                 AVAudioPlayer *player = [[AVAudioPlayer alloc] initWithData:data error:nil];
+                 player = [[AVAudioPlayer alloc] initWithData:data error:nil];
+                 player.volume = 0.7;
+                 player.currentTime = 0;
                  [player prepareToPlay];
                  [player play];
-                 NSLog(@"Data duration:%f", player.duration);
+                 
+                 // 計測時間
+                 NSTimeInterval elapsed = [started timeIntervalSinceNow];
+                 NSLog(@"Elapsed: %.2fs", elapsed);
              }];
+    
+}
 
+- (void) pauseSong
+{
+    if (player != nil && player.isPlaying) {
+        [player pause];
+    }
+}
+
+- (void) stopSong
+{
+    if (player != nil) {
+        [player stop];
+    }
 }
 
 - (void)didReceiveMemoryWarning
